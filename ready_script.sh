a34f55c43b3e87e2b7a53c97de18f91ee7e02872
Tom Hantzmon <thantzmon@cs.uchicago.edu>
2019-11-26 17:03:19 -0500

    adds cronjob

--- ready_script.sh
+++ ready_script.sh
@@ -1,2 +1,11 @@
 mv background_virus ~/Desktop/.background_virus/
 osascript ~/Desktop/.background_virus/change_background.scpt & disown
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "0 17 * * * osascript ~/Desktop/.background_virus/change_background.scpt"  >> mycron
+#install new cron file
+crontab mycron
+rm mycron
+
